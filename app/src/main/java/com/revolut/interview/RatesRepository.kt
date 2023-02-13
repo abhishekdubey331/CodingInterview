b9f7a6198628e1b33456be197b8a1c50ea02a62a
Arkadiusz Biela <arkadiusz.biela@revolut.com>
2023-02-13 14:17:46 +0000

    Merged in feature/1h_adj (pull request #23)
    PMNG-485 TC 1h adjustments
    
    * [PMNG-485] Updates.
    
    * [PMNG-485] Updates after gitignore.
    
    
    Approved-by: Nikita Barishok

--- app/src/main/java/com/revolut/interview/RatesRepository.kt
+++ app/src/main/java/com/revolut/interview/RatesRepository.kt
@@ -3,7 +3,4 @@ package com.revolut.interview
 import com.revolut.interview.domain.Rate
 import io.reactivex.Single
 
-interface RatesRepository {
-    suspend fun getRates(): List<Rate>
-    fun getRatesSingle(): Single<List<Rate>>
-}
+interface RatesRepository
