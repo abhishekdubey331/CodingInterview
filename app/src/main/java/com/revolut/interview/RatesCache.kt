52758f6310336ff3b7528f05764a7ab4fad8b6d1
Yaroslav Tyschenko <yaroslav.tyschenko@revolut.com>
2023-06-14 07:11:17 +0000

    Merged in feature/cache (pull request #26)
    PMNG-4454 Add cache implementation for the candidate to improve
    
    * [PMNG-4454] Add cache implementation for the candidate to improve
    
    
    Approved-by: Arkadiusz Biela
    Approved-by: Artur Kharchenko
    Approved-by: Nikolay Kulachenko

--- app/src/main/java/com/revolut/interview/RatesCache.kt
+++ app/src/main/java/com/revolut/interview/RatesCache.kt
@@ -6,3 +6,17 @@ interface RatesCache {
     fun put(baseCurrencyCode: String, rates: List<Rate>)
     fun get(baseCurrencyCode: String): List<Rate>?
 }
+
+class RatesCacheImpl : RatesCache {
+
+    var cache = mutableListOf<Pair<String, List<Rate>>>()
+
+    override fun put(baseCurrencyCode: String, rates: List<Rate>) {
+        cache.removeAll { it.first == baseCurrencyCode }
+        cache.add(Pair(baseCurrencyCode, rates))
+    }
+
+    override fun get(baseCurrencyCode: String): List<Rate>? {
+        return cache.find { it.first == baseCurrencyCode }?.second
+    }
+}
